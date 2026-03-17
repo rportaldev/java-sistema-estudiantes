c41fb6cfe1db3138364c3c307ad4b30277527c55
Dev Zero <litkaponii@gmail.com>
2026-03-16 21:57:10 -0500

    Agrego clase Estudiante con atributos, constructor, getters, setters y toString

--- sistema_estudiantes/src/com/sistema/estudiantes/Estudiante.java
+++ sistema_estudiantes/src/com/sistema/estudiantes/Estudiante.java
@@ -2,4 +2,69 @@ package com.sistema.estudiantes;
 
 public class Estudiante {
 
+	private int codigo;
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private String carrera;
+	
+	public Estudiante(int codigo, String nombre, String apellido, int edad, String carrera) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.carrera = carrera;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [Codigo=" + codigo + 
+				", Nombre=" + nombre + 
+				", Apellido=" + apellido + 
+				", Edad=" + edad + 
+				", Carrera=" + carrera + "]";
+	}
+	
+	
 }
