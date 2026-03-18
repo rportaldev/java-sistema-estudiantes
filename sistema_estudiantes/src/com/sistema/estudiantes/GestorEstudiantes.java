a68f3e9c30a0ab5ccc04d07b70fcb36b24659d98
Dev Zero <litkaponii@gmail.com>
2026-03-17 22:06:00 -0500

    Agrego clase GestorEstudiante con todos los metodos

--- sistema_estudiantes/src/com/sistema/estudiantes/GestorEstudiantes.java
+++ sistema_estudiantes/src/com/sistema/estudiantes/GestorEstudiantes.java
@@ -1,5 +1,62 @@
 package com.sistema.estudiantes;
 
+import java.util.ArrayList;
+
+
+
 public class GestorEstudiantes {
 
+	private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+	
+	//REGISTRAR UN ESTUDIANTE
+	public boolean registrarAlumno(Estudiante estudiante) {
+		
+		for(Estudiante e : estudiantes) {
+			
+			if(e.getCodigo() == estudiante.getCodigo()) {
+				return false;
+			}
+		}
+		
+		if(estudiante.getEdad() <= 0) {
+			return false;
+		}
+		
+		estudiantes.add(estudiante);
+		
+		return true;
+	}
+	
+	
+	//LISTAR ESTUDIANTES
+	public ArrayList<Estudiante> listarAlumnos() {
+		
+		return estudiantes;
+	}
+	
+	
+	//BUSCAR ESTUDIANTE
+	public Estudiante buscarAlumno(int codigo) {
+		
+		for(Estudiante e : estudiantes ) {
+			
+			if(e.getCodigo() == codigo) {
+				 return e;
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	//ELIMINAR UN ESTUDIANTE
+	public void eliminarAlumno(int codigo) {
+		
+		for(Estudiante e : estudiantes ) {
+			
+			if(e.getCodigo() == codigo) {
+				estudiantes.remove(e);
+			}
+		}
+	}
 }
